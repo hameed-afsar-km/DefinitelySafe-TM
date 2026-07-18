8bdb795b13a610bef7144409d05244b619458dc1
hameed-afsar-km <hameedafsar2006@gmail.com>
2026-07-19 00:04:26 +0530

    fix: Vercel error

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -9,7 +9,6 @@ export default function SmoothScroll({ children }: { children: React.ReactNode }
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       touchMultiplier: 2,
-      smoothTouch: true,
     });
 
     function raf(time: number) {
